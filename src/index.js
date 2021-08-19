73e73a148bcad5e284368bd627dc71d9a23ad5a7
Yuri An <yuriandev@gmail.com>
2021-08-19 15:15:28 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (typeof array !== "undefined" && array.length > 0) {
+        return Math.min(...array);
+    } else return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (typeof array !== "undefined" && array.length > 0) {
+        return Math.max(...array);
+    } else return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (typeof array !== "undefined" && array.length > 0) {
+        return array.reduce((a, b) => a + b) / array.length;
+    } else return 0;
+};
